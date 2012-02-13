2316f01f378a9c6495fb1857c04ca66906feaf60
Adam Ernst <adamjernst@me.com>
2012-02-13 18:28:55 -0500

    Improve signup list

--- wonderhop/landing/admin.py
+++ wonderhop/landing/admin.py
@@ -11,10 +11,14 @@ class InviteInline(admin.TabularInline):
     model = Invite
     readonly_fields = ("recipient", "sent_date",)
     extra = 0
+def num_invites_sent(signup):
+    return signup.sent_invites_set.count()
+num_invites_sent.short_description = "Invites Sent"
 class SignupAdmin(admin.ModelAdmin):
     inlines = [
         InviteInline,
     ]
+    list_display = ("email", "sign_up_date", "referring_user", num_invites_sent)
     readonly_fields = ("sign_up_date",)
 admin.site.register(Signup, SignupAdmin)
 
