41283bdebdf54d24445faa51293c92ecb805cc21
Adam Ernst <adamjernst@me.com>
2012-02-13 20:21:17 -0500

    Show number of referred users in admin

--- wonderhop/landing/admin.py
+++ wonderhop/landing/admin.py
@@ -14,11 +14,14 @@ class InviteInline(admin.TabularInline):
 def num_invites_sent(signup):
     return signup.sent_invites_set.count()
 num_invites_sent.short_description = "Invites Sent"
+def num_referred_users(signup):
+    return signup.referred_user_set.count()
+num_referred_users.short_description = "Referred Users"
 class SignupAdmin(admin.ModelAdmin):
     inlines = [
         InviteInline,
     ]
-    list_display = ("email", "sign_up_date", "referring_user", num_invites_sent)
+    list_display = ("email", "sign_up_date", "referring_user", num_invites_sent, num_referred_users)
     readonly_fields = ("sign_up_date",)
 admin.site.register(Signup, SignupAdmin)
 
